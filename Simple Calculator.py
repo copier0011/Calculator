7e2fb36098ae723d4bc14b763da22c1991ba588d
coPier0099 <102232584+copier0011@users.noreply.github.com>
2024-06-01 06:51:08 -0700

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,10 @@
+import math
+import os
+a = input("Enter your favourite first number:")
+b = input("Enter your favourite second number:")
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+
+print("Thank you for using my simple program!")
